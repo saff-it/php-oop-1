f8a50174525ffa743b84848bd517331826ac8966
saff-it <saff_it@ymail.com>
2022-09-02 16:29:47 +0200

    methods created

--- index.php
+++ index.php
@@ -1,23 +1,33 @@
 
 <?php 
     class Movie{
-        private $name;
+        private $title;
         private $genre;
         private $year;
 
-        function __construct($name, $genre, $year){
-
+        function __construct($_title,$_genre,$_year){
+            $this->setTitle($_title);
+            $this->genre = $_genre;
+            $this->year = $_year;
         }
 
-        
+        public function setTitle($title){
+            if (strlen($title) > 3){
+                $this->title = $title;
+            } else {
+                $this->title = 'Film inesitente';
+            }
+        }
+
+        public function getYear(){
+            return $this->year;
+        }
+
     }
 
 
-
-
-    $movieOne = new Movie();
-    $movieTwo = new Movie();
-    
+    $movieOne = new Movie('Titanic', 'horror', '2002');
+    var_dump($movieOne);
 
 ?>
 
